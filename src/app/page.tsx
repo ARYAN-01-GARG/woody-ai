f9c6e053477f0803191515fbc7ff0290d013b115
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-07-31 16:40:48 +0530

    Add : TRPC preftech base tanstack integration complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { getQueryClient, trpc } from '@/trpc/server';
 import { ClientGreeting } from './client-greeting';
+import { Suspense } from 'react';
 export default async function Home() {
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(
     trpc.hello.queryOptions({
-      text: 'world',
+      text: 'Hello from the server!',
     }),
   );
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <div>...</div>
-      {/** ... */}
-      <ClientGreeting />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ClientGreeting />
+      </Suspense>
     </HydrationBoundary>
   );
 }
--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,10 +1,11 @@
 'use client';
-// ^-- to make sure we can mount the Provider from a server component
+
 import type { QueryClient } from '@tanstack/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import { createTRPCContext } from '@trpc/tanstack-react-query';
 import { useState } from 'react';
+import superjson from 'superjson';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
@@ -24,26 +25,23 @@ function getQueryClient() {
 function getUrl() {
   const base = (() => {
     if (typeof window !== 'undefined') return '';
-    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-    return 'http://localhost:3000';
+    return process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
   })();
   return `${base}/api/trpc`;
 }
+
 export function TRPCReactProvider(
   props: Readonly<{
     children: React.ReactNode;
   }>,
 ) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
+
   const queryClient = getQueryClient();
   const [trpcClient] = useState(() =>
     createTRPCClient<AppRouter>({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
           url: getUrl(),
         }),
       ],
