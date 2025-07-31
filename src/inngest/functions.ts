0796790edda6de88d1bbb49c4623c1b07e738d8a
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-07-31 21:20:27 +0530

    Done Open AI API integration

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,20 @@
 import { inngest } from "./client";
+import { openai, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+   async ({ event }) => {
+    const writer = createAgent({
+      name: "writer",
+      system: "You are an expert writer.  You write readable, concise, simple content.",
+      model: openai({ model: "gpt-4o"}),
+    });
+
+    const { output } = await writer.run(`Write a tweet on : ${event.data.topic}`);
+
+    console.log("Output from writer:", output);
+
+    return output;
+  }
 );
