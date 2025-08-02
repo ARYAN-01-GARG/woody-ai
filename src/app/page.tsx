2ae16bf06efebd27a053b1c63ccbfa5094be2148
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-08-02 23:11:43 +0530

    Add : added message and fragment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,16 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Home() {
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({
+  const { data : messages } = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage = useMutation(trpc.messages.create.mutationOptions({
     onSuccess: () => {
-      toast.success("Function invoked successfully!");
+      toast.success("Message Sent successfully!");
     }
   }));
   const [value, setValue] = useState("");
@@ -19,9 +20,10 @@ export default function Home() {
   return (
     <div>
       <Input value={value} onChange={(e) => setValue(e.target.value)} placeholder="Enter a value" />
-      <Button onClick={() => invoke.mutate({ value })} disabled={invoke.isPending}>
-        Invoke Function
+      <Button onClick={() => createMessage.mutate({ value })} disabled={createMessage.isPending}>
+        Send Message
       </Button>
+      {JSON.stringify(messages, null, 2)}
     </div>
   );
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -3,12 +3,10 @@ import { inngest } from "./client";
 import { openai, createAgent, createTool, createNetwork } from "@inngest/agent-kit";
 import { getSandbox, lastAssistantTextMessageContent } from "./utils";
 import { PROMPT } from "@/prompt";
-import { title } from "process";
-import { SunMedium } from "lucide-react";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+export const codeAgentFunction = inngest.createFunction(
+  { id: "codeAgentFunction" },
+  { event: "code-agent" },
    async ({ event , step }) => {
     const sandboxId = await step.run("get-sandbox-id", async () => {
       const sandbox = await Sandbox.create("woody-nestjs-temp");
@@ -17,7 +15,7 @@ export const helloWorld = inngest.createFunction(
 
     try {
       const codeAgent = createAgent({
-        name: "writer",
+        name: "codeAgent",
         system: PROMPT,
         description: "An AI Expert coding agent that can create a nextjs project",
         model: openai({ model: "gpt-4o"}),
